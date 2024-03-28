5b34e29b8c7cd635eaa37bfba3b931a50cde7f20
Aibek Ayazbek <aayazbek1936@gmail.com>
2024-03-28 16:50:51 +0500

    for Header

--- components/modules/Header/HeaderTop.tsx
+++ components/modules/Header/HeaderTop.tsx
@@ -1,52 +1,103 @@
 import CityButton from '@/components/elements/CityButton/CityButton'
-import styles from '@/styles/header/index.module.scss'
+import ModeToggler from '@/components/elements/ModeToggler/ModeToggler'
 import Link from 'next/link'
+import ProfileDropdown from './ProfileDropdown'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
+import { useStore } from 'effector-react'
+import { $mode } from '@/context/mode'
+import { usePopup } from '@/hooks/usePopup'
+import styles from '@/styles/header/index.module.scss'
 
 const HeaderTop = () => {
-  console.log('')
+  const isMedia950 = useMediaQuery(950)
+  const { toggleOpen, open, closePopup } = usePopup()
+  const mode = useStore($mode)
+  const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
   return (
     <div className={styles.header__top}>
       <div className={`container ${styles.header__top__container}`}>
-        <CityButton />
-        <nav className={styles.header__nav}>
+        {!isMedia950 && <CityButton />}
+        {isMedia950 && (
+          <button
+            onClick={toggleOpen}
+            className={`${styles.burger_menu} ${
+              open ? styles.open : ''
+            } ${darkModeClass}`}
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+        )}
+        <nav
+          className={`${styles.header__nav} ${
+            open ? styles.open : ''
+          } ${darkModeClass}`}
+        >
           <ul className={styles.header__nav__list}>
             <li className={styles.header__nav__list__item}>
-              <Link href="shopping-payment" passHref legacyBehavior>
-                <a className={styles.header__nav__list__item__link}>
+              <Link href="/shipping-payment" passHref legacyBehavior>
+                <a
+                  className={`${styles.header__nav__list__item__link} ${darkModeClass}`}
+                  onClick={closePopup}
+                >
                   Доставка и оплата
                 </a>
               </Link>
             </li>
             <li className={styles.header__nav__list__item}>
               <Link href="/about" passHref legacyBehavior>
-                <a className={`${styles.header__nav__list__item__link} `}>
+                <a
+                  className={`${styles.header__nav__list__item__link} ${darkModeClass}`}
+                  onClick={closePopup}
+                >
                   О компании
                 </a>
               </Link>
             </li>
             <li className={styles.header__nav__list__item}>
               <Link href="/catalog" passHref legacyBehavior>
-                <a className={`${styles.header__nav__list__item__link} `}>
+                <a
+                  className={`${styles.header__nav__list__item__link} ${darkModeClass}`}
+                  onClick={closePopup}
+                >
                   Каталог
                 </a>
               </Link>
             </li>
             <li className={styles.header__nav__list__item}>
               <Link href="/contacts" passHref legacyBehavior>
-                <a className={`${styles.header__nav__list__item__link} `}>
+                <a
+                  className={`${styles.header__nav__list__item__link} ${darkModeClass}`}
+                  onClick={closePopup}
+                >
                   Контакты
                 </a>
               </Link>
             </li>
             <li className={styles.header__nav__list__item}>
               <Link href="/wholesale-buyers" passHref legacyBehavior>
-                <a className={`${styles.header__nav__list__item__link} `}>
+                <a
+                  className={`${styles.header__nav__list__item__link} ${darkModeClass}`}
+                  onClick={closePopup}
+                >
                   Оптовым покупателям
                 </a>
               </Link>
             </li>
+            {isMedia950 && (
+              <li className={styles.header__nav__list__item}>
+                <CityButton />
+              </li>
+            )}
+            {isMedia950 && (
+              <li className={styles.header__nav__list__item}>
+                <ModeToggler />
+              </li>
+            )}
           </ul>
         </nav>
+        <ProfileDropdown />
       </div>
     </div>
   )
