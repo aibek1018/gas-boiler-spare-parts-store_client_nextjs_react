615dc78b43b0e46e622fbe8996bf30ce1d268b48
Aibek Ayazbek <aayazbek1936@gmail.com>
2024-03-29 17:37:17 +0500

    CatalogPage

---
+++ components/templates/CatalogPage/CatalogPage.tsx
@@ -0,0 +1,201 @@
+import { getBoilerPartsFx } from '@/app/api/boilerParts'
+import FilterSelect from '@/components/modules/CatalogPage/FilterSelect'
+import ManufacturersBlock from '@/components/modules/CatalogPage/ManufacturersBlock'
+import { $boilerParts, setBoilerParts } from '@/context/boilerParts'
+import { $mode } from '@/context/mode'
+import { useStore } from 'effector-react'
+import { AnimatePresence } from 'framer-motion'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import styles from '@/styles/catalog/index.module.scss'
+import skeletonStyles from '@/styles/skeleton/index.module.scss'
+import CatalogItem from '@/components/modules/CatalogPage/CatalogItem'
+import ReactPaginate from 'react-paginate'
+import { IQueryParams } from '@/types/catalog'
+import { useRouter } from 'next/router'
+import { IBoilerParts } from '@/types/boilerparts'
+
+const CatalogPage = ({ query }: { query: IQueryParams }) => {
+  const mode = useStore($mode)
+  const boilerParts = useStore($boilerParts)
+  const [spinner, setSpinner] = useState(false)
+  const pagesCount = Math.ceil(boilerParts.count / 20)
+  const isValidOffset =
+    query.offset && !isNaN(+query.offset) && +query.offset > 0
+  const [currentPage, setCurrentPage] = useState(
+    isValidOffset ? +query.offset - 1 : 0
+  )
+  const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
+  const router = useRouter()
+
+  useEffect(() => {
+    loadBoilerParts()
+  }, [])
+
+  console.log(boilerParts.rows)
+
+  const loadBoilerParts = async () => {
+    try {
+      setSpinner(true)
+      const data = await getBoilerPartsFx('/boiler-parts?limit=20&offset=0')
+
+      if (!isValidOffset) {
+        router.replace({
+          query: {
+            offset: 1,
+          },
+        })
+
+        resetPagination(data)
+        return
+      }
+
+      if (isValidOffset) {
+        if (+query.offset > Math.ceil(data.count / 20)) {
+          router.push(
+            {
+              query: {
+                ...query,
+                offset: 1,
+              },
+            },
+            undefined,
+            { shallow: true }
+          )
+
+          setCurrentPage(0)
+          setBoilerParts(data)
+          return
+        }
+      }
+
+      const offset = +query.offset - 1
+      const result = await getBoilerPartsFx(
+        `/boiler-parts?limit=20&offset=${offset}`
+      )
+      setCurrentPage(offset)
+      setBoilerParts(result)
+    } catch (error) {
+      toast.error((error as Error).message)
+    } finally {
+      setSpinner(false)
+    }
+  }
+
+  const resetPagination = (data: IBoilerParts) => {
+    setCurrentPage(0)
+    setBoilerParts(data)
+  }
+
+  const handlePageChange = async ({ selected }: { selected: number }) => {
+    try {
+      const data = await getBoilerPartsFx('/boiler-parts?limit=20&offset=0')
+
+      if (selected > pagesCount) {
+        resetPagination(data)
+        return
+      }
+
+      if (isValidOffset && +query.offset > Math.ceil(data.count / 2)) {
+        resetPagination(data)
+        return
+      }
+
+      // const { isValidBoilerQuery, isValidPartsQuery, isValidPriceQuery } =
+      //   checkQueryParams(router)
+
+      const result = await getBoilerPartsFx(
+        `/boiler-parts?limit=20&offset=${selected}`
+      )
+
+      router.push(
+        {
+          query: {
+            ...router.query,
+            offset: selected + 1,
+          },
+        },
+        undefined,
+        { shallow: true }
+      )
+
+      setCurrentPage(selected)
+      setBoilerParts(result)
+    } catch (error) {
+      toast.error((error as Error).message)
+    } finally {
+      setTimeout(() => setSpinner(false), 1000)
+    }
+  }
+
+  return (
+    <section className={styles.catalog}>
+      <div className={`container ${styles.catalog__container}`}>
+        <h2 className={`${styles.catalog__title} ${darkModeClass}`}>
+          Каталог товаров
+        </h2>
+        <div className={`${styles.catalog__top} ${darkModeClass}`}>
+          <AnimatePresence>
+            {false && <ManufacturersBlock title="Производитель котлов:" />}
+          </AnimatePresence>
+          <AnimatePresence>
+            {false && <ManufacturersBlock title="Производитель запчастей:" />}
+          </AnimatePresence>
+          <div className={styles.catalog__top__inner}>
+            <button
+              className={`${styles.catalog__top__reset} ${darkModeClass}`}
+              disabled={true}
+            >
+              Сбросить фильтр
+            </button>
+            <FilterSelect />
+          </div>
+        </div>
+        <div className={styles.catalog__bottom}>
+          <div className={styles.catalog__bottom__inner}>
+            <div className="">Filters</div>
+            {spinner ? (
+              <ul className={skeletonStyles.skeleton}>
+                {Array.from(new Array(8)).map((_, i) => (
+                  <li
+                    key={i}
+                    className={`${skeletonStyles.skeleton__item} ${
+                      mode === 'dark' ? `${skeletonStyles.dark_mode}` : ''
+                    }`}
+                  >
+                    <div className={skeletonStyles.skeleton__item__light} />
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <ul className={styles.catalog__list}>
+                {boilerParts.rows?.length ? (
+                  boilerParts.rows.map((item) => (
+                    <CatalogItem item={item} key={item.id} />
+                  ))
+                ) : (
+                  <span>Список товаров пуст...</span>
+                )}
+              </ul>
+            )}
+          </div>
+          <ReactPaginate
+            containerClassName={styles.catalog__bottom__list}
+            pageClassName={styles.catalog__bottom__list__item}
+            pageLinkClassName={styles.catalog__bottom__list__item__link}
+            previousClassName={styles.catalog__bottom__list__prev}
+            nextClassName={styles.catalog__bottom__list__next}
+            breakClassName={styles.catalog__bottom__list__break}
+            breakLinkClassName={`${styles.catalog__bottom__list__break__link} ${darkModeClass}`}
+            breakLabel="..."
+            pageCount={pagesCount}
+            forcePage={currentPage}
+            onPageChange={handlePageChange}
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default CatalogPage
