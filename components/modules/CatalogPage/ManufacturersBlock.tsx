a7a836c77428c857adcabae80cace92f063303a5
Aibek Ayazbek <aayazbek1936@gmail.com>
2024-04-03 09:29:52 +0500

    manufacturersBlock

--- components/modules/CatalogPage/ManufacturersBlock.tsx
+++ components/modules/CatalogPage/ManufacturersBlock.tsx
@@ -1,26 +1,35 @@
-import { $mode } from '@/context/mode'
 import { useStore } from 'effector-react'
-import { motion } from 'framer-motion'
-import styles from '@/styles/catalog/index.module.scss'
+import { $mode } from '@/context/mode'
+import { AnimatePresence, motion } from 'framer-motion'
 import { IManufacturersBlockProps } from '@/types/catalog'
+import ManufacturersBlockItem from './ManufacturersBlockItem'
+import styles from '@/styles/catalog/index.module.scss'
 
-const ManufacturersBlock = ({ title }: IManufacturersBlockProps) => {
+const ManufacturersBlock = ({
+  title,
+  manufacturersList,
+  event,
+}: IManufacturersBlockProps) => {
   const mode = useStore($mode)
   const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
+  const checkedItems = manufacturersList.filter((item) => item.checked)
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className={`${styles.dashboard__alert} ${darkModeClass}`}
+      className={`${styles.manufacturers} ${darkModeClass}`}
     >
       <h3 className={`${styles.manufacturers__title} ${darkModeClass}`}>
         {title}
       </h3>
-      <ul>
-        {[].map((item) => (
-          <li key={item} />
-        ))}
+      <ul className={styles.manufacturers__list}>
+        <AnimatePresence>
+          {checkedItems.map((item) => (
+            <ManufacturersBlockItem key={item.id} item={item} event={event} />
+          ))}
+        </AnimatePresence>
       </ul>
     </motion.div>
   )
